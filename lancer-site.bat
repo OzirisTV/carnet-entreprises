595b5b128847516872f29e733dec25976675e6ae
OzirisTV <kevan799@gmail.com>
2026-06-20 01:51:08 +0200

    Delete lancer-site.bat

--- lancer-site.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "NODE_EXE=C:\Users\Kevan\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe"
-if not exist "%NODE_EXE%" set "NODE_EXE=node"
-cd /d "%~dp0"
-echo Demarrage du carnet DHL...
-echo.
-"%NODE_EXE%" server.js
-pause
